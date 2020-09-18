9d31ffd622991c82def051aac1e2e36306daeace
Foppe <flundkvi@kth.se>
2020-09-18 11:33:34 +0200

    implement basic physics for thrown balls

---
+++ Assets/Scripts/Component Systems/ThrowMotionSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Physics;
+using UnityEngine;
+
+/**
+    Simple implementation of a throw motion, works on throwable components
+**/
+public class ThrowMotionSystem : JobComponentSystem {
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps){
+        float time = (float) Time.ElapsedTime;
+        Entities.ForEach((ref PhysicsVelocity velocity, ref Translation position, in Throwable throwable) => { 
+            //calculate Y velocity
+            velocity.Linear.y  = throwable.initialVelocity * math.sin(throwable.angle) - (math.abs(Physics.gravity.y) * time); //use abs since gravity is negative sometimes
+            //calculate X velocity
+            velocity.Linear.x = throwable.initialVelocity * math.cos(throwable.angle);
+
+        }).Run();
+
+        return default;
+    }   
+}
---
+++ Assets/Scripts/DataTypes/Throwable.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct Throwable :  IComponentData {
+    public float angle;
+    public float initialVelocity;
+}
