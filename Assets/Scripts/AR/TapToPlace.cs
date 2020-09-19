d2dc44e01a6b2fd0d64dbfcc9cdb3969b80c73ff
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2020-09-19 20:29:05 +0200

    Started AR Foundation integration

---
+++ Assets/Scripts/AR/TapToPlace.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TapToPlace : MonoBehaviour
+{
+    public GameObject placementIndicator;
+
+    private ARRaycastManager raycastManager;
+    private Pose placementPose;
+    private bool validPlacementPose = false;
+
+    void Start()
+    {
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        validPlacementPose = hits.Count > 0;
+
+        if (validPlacementPose)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (validPlacementPose)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+}
