4190ae2bbaf7c6dff4a82f47f4d3b022002214d4
Mattias Larsson <matlar4@kth.se>
2020-09-24 15:52:05 +0200

    buttons for choosing what scene to start

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuScript : MonoBehaviour
+public class MenuScript : MonoBehaviour
 {
-    public void StartGame()
+    public void StartNonARDemo()
     {
         SceneManager.LoadScene(1); //Scenemanager.getcurrentscene()+1 TYP SÅ FÖR ATT GÅ TILL NÄSTA I KÖ
 
     }
-
 }
