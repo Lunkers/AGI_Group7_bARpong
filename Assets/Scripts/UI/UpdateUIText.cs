00b7155b873513202714a1e99f00e2fea8f9a6e1
Mattias Larsson <matlar4@kth.se>
2020-09-29 17:00:30 +0200

    function for updating UI

---
+++ Assets/Scripts/UI/UpdateUIText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour
+{
+    public Text UIText;
+    public int curScore = 0;
+
+    public void UpdateText()
+    {
+        curScore = curScore + 100;
+        UIText.text = "Score: " + curScore.ToString();
+    }
+}
