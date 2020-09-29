d9678623cca928dd91a7565e957de57ed53e806e
Mattias Larsson <matlar4@kth.se>
2020-09-29 11:27:49 +0200

    UI and prefabs and relocate files

---
+++ Assets/Scripts/UI/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartNonARDemo()
+    {
+        SceneManager.LoadScene(1); //Numret här är numret i queuen under build settings
+
+    }
+    public void StartArSwipeDemo()
+    {
+        SceneManager.LoadScene(2); 
+
+    }
+    public void StartArButtonDemo()
+    {
+        SceneManager.LoadScene(3);
+
+    }
+}
