3cc7d76aa03c5ed1493e20caa708e1669045ab24
Foppe <flundkvi@kth.se>
2020-11-09 11:48:53 +0100

    quick hack for fixed rate physics

---
+++ Assets/Scripts/Component Systems/PhysicsFixedRateHack.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public class PhysicsFixedRateHack : SystemBase
+{
+  private bool _IsInitialized;
+
+  protected override void OnUpdate() {
+      if(!_IsInitialized){
+          FixedRateUtils.EnableFixedRateWithCatchUp(
+              World.GetOrCreateSystem<SimulationSystemGroup>(),
+              UnityEngine.Time.fixedDeltaTime);
+              _IsInitialized = true;
+      }
+  }
+}
