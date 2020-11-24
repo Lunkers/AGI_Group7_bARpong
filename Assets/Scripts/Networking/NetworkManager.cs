d4822799bf58d740463b0efb9ddcc780c2c104f3
bonnes1 <hannes.bennet@gmail.com>
2020-11-24 19:06:19 +0100

    loading same scene and scene ends when one player hits all cups

--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -1,5 +1,6 @@
 ﻿using Photon.Pun;
 using Photon.Realtime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -15,18 +16,16 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     [SerializeField] private TMP_InputField hostRoomInputfield = null;
     [SerializeField] private Button hostButton = null;
+    public TMP_Text error;
 
     private const int MaxPlayersPerRoom = 2;
 
     private void Awake()
     {
-        //PhotonNetwork.AutomaticallySyncScene = true; // senast ingen scen för någon av användarna, men denna skall användas någonstans
-    }
-    void Start()
-    {
         PhotonNetwork.ConnectUsingSettings();
-        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.AutomaticallySyncScene = true; // senast ingen scen för någon av användarna, men denna skall användas någonstans
     }
+
 
     public override void OnConnectedToMaster() => Debug.Log("Connected to Master");
     public void JoinRoom()
@@ -36,21 +35,30 @@ public class NetworkManager : MonoBehaviourPunCallbacks
        
 
         PhotonNetwork.JoinRoom(RoomName);
-        
+        //Check if room do not exist - Display it! 
       
         Debug.Log("Attempting to connect to room:  " + RoomName);
     }
 
     public void CreateRoom()
     {
+        bool isLoading = false;
         string RoomName = hostRoomInputfield.text;
         PhotonNetwork.NickName = RoomName;
-
+        
         RoomOptions options = new RoomOptions();
-        options.MaxPlayers = 2;
+        options.MaxPlayers = MaxPlayersPerRoom;
         PhotonNetwork.CreateRoom(RoomName, options, TypedLobby.Default);
         Debug.Log("Inside createRoom function and created room: " + RoomName);
         
+    }
+
+    public void LeaveOnReturn()
+    {
+        PhotonNetwork.Disconnect();
+        Debug.Log("Disconnecting from server.");
+        PhotonNetwork.ConnectUsingSettings();
+        //Destroy playerObject needed? 
     }
 
     //When opponent has joined
@@ -59,7 +67,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         Debug.Log("Client successfully joined a room.");
 
         int numberOfPlayers = PhotonNetwork.CurrentRoom.PlayerCount;
-
+       
         if (numberOfPlayers != MaxPlayersPerRoom)
         {
 
@@ -70,7 +78,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         {
 
             Debug.Log("Matching is ready to begin!");
-            PhotonNetwork.LoadLevel("SampleScene");
+            
         }
     }
 
@@ -80,23 +88,26 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         {
             PhotonNetwork.CurrentRoom.IsOpen = false;
             Debug.Log("Match is ready to begin.");
-
-
-            //PhotonNetwork.LoadLevel("Beer_game");
+            
+            if (PhotonNetwork.IsMasterClient) 
+            {
+                try {
+                    PhotonNetwork.LoadLevel("ARTest_Multiplayer");
+                } 
+                catch (Exception e)  {
+                    error.text = e.ToString();
+                }
+               
+                
+            }
+            
         }
     }
     public override void OnDisconnected(DisconnectCause cause)
     {
-      
-
         Debug.Log("Disconnected due to: " + cause);
     }
 
-
-
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
