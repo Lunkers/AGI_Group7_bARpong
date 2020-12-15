2c3ffa4af7978ee6819f0118b84e302f0731cbc7
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2020-12-15 14:07:23 +0100

    Added reset button

---
+++ Assets/Scripts/UI/ResetButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class ResetButton : MonoBehaviour
+{
+    private ThrowMotionSystem throwMotionSystem;
+
+    void Start()
+    {
+        throwMotionSystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<ThrowMotionSystem>();
+    }
+
+    public void HandleResetButtonPressed()
+    {
+        throwMotionSystem.Reset();
+    }
+}
